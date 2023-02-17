4bff017c5dcc7fc8fcb901ef40011fa17040b004
Zeio Nara <zeionara@gmail.com>
2023-02-17 20:04:04 +0000

    Added git support for gpg script

--- gpg.sh
+++ gpg.sh
@@ -3,21 +3,48 @@
 name='Zeio Nara'
 email='zeionara@gmail.com'
 
-gpgen () {
-    gpg2 --version || sudo apt-get install gnupg2
-
-    if [[ -z $1 ]]; then
-        gpg2 --quick-generate-key "$name <$email>" rsa4096 cert never
-    else
-        gpg2 --quick-generate-key "$name ($1) <$email>" rsa4096 cert never
-    fi
-
+gpga () {
     key=$(gpg2 --list-secret-keys --keyid-format LONG $email | grep sec | sed 's/.*rsa4096\///' | sed 's/ .*//' | tail -n 1)
 
     echo -e "\nKey id: $key\n"
 
     gpg2 --armor --export $key
+
+    export __GPG_KEY=$key
+}
+
+gpgen () {
+    gpg2 --version || sudo apt-get install gnupg2
+
+    if [[ -z $1 ]]; then
+        gpg2 --quick-generate-key "$name <$email>" rsa4096 sign,encrypt never
+    else
+        gpg2 --quick-generate-key "$name ($1) <$email>" rsa4096 sign,encrypt never
+    fi
+
+    gpga
+
+    # key=$(gpg2 --list-secret-keys --keyid-format LONG $email | grep sec | sed 's/.*rsa4096\///' | sed 's/ .*//' | tail -n 1)
+
+    # echo -e "\nKey id: $key\n"
+
+    # gpg2 --armor --export $key
+
+    # export __GPG_KEY=$key
+}
+
+gpgeng () {
+    gpgen $1
+
+    git config --global user.singingkey $__GPG_KEY
+    git config --global commit.gpgsign true
+
+    git config --global user.name "$name"
+    git config --global user.email "$email"
+
+    git config --global gpg.program gpg2
 }
 
 alias gpgl="gpg2 --list-secret-keys --keyid-format LONG $email"
+
 alias gpgenf="gpg2 --full-generate-key"
