bd8c75dfab0c02abf2820058acddc7151e86abf8
zeio <zeionara@gmail.com>
2023-10-15 22:11:39 +0300

    feat(etc): added etc tools

---
+++ etc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# colors
+
+export GREEN=10
+export BLUE=12
+export RED=196
+export FUCHSIA=213
+export YELLOW=226
+
+alias palette='for code in {000..255}; do print -P -- "$code: %B%F{$code}Color%f"; done'
+
+color () {
+    code=$1
+
+    if [[ $code -lt 0 ]] || [[ $code -gt 255 ]]; then
+        echo "Color code must be in the interval [0; 255]"
+        return
+    fi
+
+    print -P "$code: %B%F{$code}Color%f"
+}
