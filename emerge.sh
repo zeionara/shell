07fea49e4c80669c4071eeeba2207592ce370410
Zeio Nara <zeionara@gmail.com>
2023-10-30 17:28:29 +0300

    fix(alias): added more aliases for emerge

--- emerge.sh
+++ emerge.sh
@@ -1,3 +1,6 @@
 eeg () {
 	sudo tail -f /var/log/emerge.log | awk -F ':  ' '{ printf "%s:  %s\n", strftime("%d-%m-%Y %H:%M:%S", $1), $2 }'
 }
+
+alias ega='sudo emerge --ask'
+alias egl='qlist -IRv'
