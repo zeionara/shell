8e8df64608690a3bc2cdaa6edfb6953da9a248f4
Zeio Nara <zeionara@gmail.com>
2023-11-02 15:36:13 +0300

    feat(emerge): added more emerge shortcuts

--- emerge.sh
+++ emerge.sh
@@ -1,6 +1,11 @@
-eeg () {
+alias ega='sudo emerge --ask'
+
+alias egd='sudo emerge --depclean'
+
+ege () {
 	sudo tail -f /var/log/emerge.log | awk -F ':  ' '{ printf "%s:  %s\n", strftime("%d-%m-%Y %H:%M:%S", $1), $2 }'
 }
 
-alias ega='sudo emerge --ask'
 alias egl='qlist -IRv'
+alias egs='sudo emerge --sync'
+alias egw='sudo emerge -av @world'
