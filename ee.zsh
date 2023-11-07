6bb4c50a41e2b954290036907d2edfb55fc02851
Zeio Nara <zeionara@gmail.com>
2023-11-07 03:25:07 +0300

    feat(ee-weight): implemented weighted ee computation strategy by considering number of occurrences in history as well as the index of last usage

--- ee.zsh
+++ ee.zsh
@@ -9,23 +9,63 @@ _spec () {
   _wanted -V values expl "$description" compadd -P '$' "$@"
 }
 
+# cat "$HOME/.zsh_history" | grep -E '^[^a-zA-Z]+echo\s+\$[A-Z_]+$' | cut -c 22- | cut -d ' ' -f 1 | sort | uniq -c | sort -nr | cut -d ' ' -f 8- | sed -E "s/(.*)/'\1'/" | tr '\n' ' '
+
+_get_ee_options () {
+  get_counts () {
+    echo "$(
+      cat "$HOME/.zsh_history" |
+      grep -E '^[^a-zA-Z]+echo\s+\$[A-Z_]+$' |
+      cut -c 22- |
+      cut -d ' ' -f 1 |
+      sort |
+      uniq -c |
+      sort -nr
+    )"
+  }
+
+  counts="$(get_counts)"
+
+  export __max_count=$(echo "$counts" | head -n 1 | awk '{print $1}')
+  export __history_length=$(cat "$HOME/.zsh_history" | wc -l)
+
+  cat "$HOME/.zsh_history" |
+  grep -E '^[^a-zA-Z]+echo\s+\$[A-Z_]+$' |
+  cut -c 22- |
+  cut -d ' ' -f 1 |
+  sort |
+  uniq -c -w1 |
+  awk '{print $2, $1}' |
+  sed -E 's#(([^ ]+).*)$#echo \1 $(tac "$HOME/.zsh_history" | grep -n "\2" | cut -d ":" -f 1 | head -n 1)#' |
+  zsh |
+  # awk '{first = $2 * 0.5; second = $3 * 0.2; sum = first + second; print $1, $2, first, $3, second, sum}'
+  awk '{first = $2 / ENVIRON["__max_count"]; second = 1 - $3 / ENVIRON["__history_length"]; count_weight = 0.3; sum = count_weight * first + (1 - count_weight) * second; print sum, $1}' |
+  sort -nr |
+  cut -d ' ' -f 2- |
+  sed -E "s/(.*)/'\1'/"
+}
+
+_ee_options=$(_get_ee_options | tr '\n' ' ')
+
 _setup_ee_completions=$(
-  echo _spec "history" $(
-    cat "$HOME/.zsh_history" |
-    grep -E '^[^a-zA-Z]+echo\s+\$[A-Z_]+$' |
-    # cut -c 21- | cut -d ' ' -f 1- |
-    cut -c 22- | cut -d ' ' -f 1 |
-    sort |
-    uniq -c |
-    sort -nr |
-    cut -d ' ' -f 8- |
-    # sed 's/"/\\"/g' |
-    # sed 's/\\n/\\\\\\\\n/g' |
-    # sed -E 's/(.*)/"\1"/' |
-    sed -E "s/(.*)/'\1'/" |
-    tr '\n' ' '
-    # sed 's/\$/\\$/g'
-  )
+  echo _spec "history" $_ee_options
+  # $(
+  #   # _get_ee_options
+  #   cat "$HOME/.zsh_history" |
+  #   grep -E '^[^a-zA-Z]+echo\s+\$[A-Z_]+$' |
+  #   # cut -c 21- | cut -d ' ' -f 1- |
+  #   cut -c 22- | cut -d ' ' -f 1 |
+  #   sort |
+  #   uniq -c |
+  #   sort -nr |
+  #   cut -d ' ' -f 8- |
+  #   # sed 's/"/\\"/g' |
+  #   # sed 's/\\n/\\\\\\\\n/g' |
+  #   # sed -E 's/(.*)/"\1"/' |
+  #   sed -E "s/(.*)/'\1'/" |
+  #   tr '\n' ' '
+  #   # sed 's/\$/\\$/g'
+  # )
 )
 # echo "$foo"
 # eval "$foo"
