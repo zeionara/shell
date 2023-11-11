46f2508ed4647318d4ff147006dae01ca35f63ae
Zeio Nara <zeionara@gmail.com>
2023-11-11 20:09:17 +0300

    feat(emerge): added ego alias for emerge

--- emerge.sh
+++ emerge.sh
@@ -7,7 +7,12 @@ ege () {
 }
 
 alias egl='qlist -IRv'
+
+alias ego="sudo emerge --oneshot"
+alias egop="sudo emerge --oneshot portage"
+
 alias egs='sudo emerge --sync'
+alias egu='sudo emerge --ask --verbose --update --deep --newuse @world'
 alias egw='sudo emerge -av @world'
 
 alias uga='sudo emerge --unmerge --ask'
