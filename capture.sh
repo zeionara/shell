ba67ccab1aaba0fa3ff37407a76ab093f01c5c2a
Zeio Nara <zeionara@gmail.com>
2023-11-28 23:58:31 +0300

    feat(web-camera): added an alias for web-camera capture through gst-launch

--- capture.sh
+++ capture.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 alias crs='wf-recorder --muxer=v4l2 --codec=rawvideo --file=/dev/video2 -t <<< y'
+
 alias rec='wf-recorder -a <<< y'
+alias recg='wf-recorder -a -g "$(slurp)" <<< y'
+
+alias wr='wf-recorder'
+alias wrg='wf-recorder -g "$(slurp)"'
+
+alias glw='gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! waylandsink'
