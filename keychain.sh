f03d8bd94fb3af405994a38bdb4aa422014c946b
Zeio Nara <zeionara@gmail.com>
2023-12-13 23:47:22 +0300

    fix(timeout): fixed gpg short lifespan

--- keychain.sh
+++ keychain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -z $(which keychain | grep -v 'not found') ] && [ ! -z ${GPG_KEY_ID} ]; then
-  keychain --timeout 600 $HOME/.ssh/id_ed25519
-  keychain --agents gpg --timeout 600 "$GPG_KEY_ID"
+  keychain $HOME/.ssh/id_ed25519
+  keychain --agents gpg "$GPG_KEY_ID"
 
   . $HOME/.keychain/${HOST}-sh
   . $HOME/.keychain/${HOST}-sh-gpg
