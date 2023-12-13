8109a26963b456aed57c89bdc47936b4540a24dd
Dmitrii <zeionara@gmail.com>
2023-12-13 23:46:31 +0300

    fix(hugging-face): added separate commands for usual cloning and cloning without large files+

--- hugging-face.sh
+++ hugging-face.sh
@@ -1,5 +1,12 @@
 hfg () {
     name=$1
 
+    # GIT_LFS_SKIP_SMUDGE=1 git clone git@hf.co:$name.git
+    git clone git@hf.co:$name.git
+}
+
+hfgs () {
+    name=$1
+
     GIT_LFS_SKIP_SMUDGE=1 git clone git@hf.co:$name.git
 }
